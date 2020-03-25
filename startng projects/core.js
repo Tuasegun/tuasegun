139cb638344ee0ebf21bdc0bc95831903e8da911
Tuasegun <59211245+Tuasegun@users.noreply.github.com>
2020-03-25 23:37:32 +0000

    Add files via upload

---
+++ startng projects/core.js
@@ -0,0 +1,10 @@
+var string = "Segun";
+let listOfCourse = ["Node.js", "php", "html-css-jscript", "c#"] 
+let number = 0;
+while (number <= 200){
+    console.log(number)
+    number = number + 2;
+
+}
+console.log(listOfCourse);
+console.log(string)
